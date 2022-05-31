c4930fd916722327af948340baa63899683f409c
Samsutheen <58632746+aepcsam@users.noreply.github.com>
2022-06-01 01:18:35 +0300

    Add files via upload

--- Assignment/9-Edaman_recipe/script.js
+++ Assignment/9-Edaman_recipe/script.js
@@ -36,17 +36,46 @@ function useApiData(results){
     <div class="card d-flex flex-auto" style="width: 18rem;">
     <img src="${result.recipe.image}" class="card-img-top" alt="...">
     <div class="card-body">
+
       <h5 class="card-title">${result.recipe.label}</h5>
-      <p class="card-text">Diet label : ${result.recipe.dietLabels}</p>
+      
+      <p class="card-text">Health label : ${result.recipe.healthLabels}</p>
+      <p class="card-text">Diet label : ${result.recipe.dietLabels.length >0 ? result.recipe.dietLabels : "Data No Found"}</p>
+      
       <p class="card-text">Calories: ${result.recipe.calories.toFixed(2)}</p>
+      
+      <table>
+        <tr>
+        <td>${result.recipe.totalNutrients.CHOLE.label}</td>
+        <td>${result.recipe.totalNutrients.CHOLE.quantity.toFixed()} ${result.recipe.totalNutrients.CHOLE.unit}</td>
+        </tr>
+        <tr>
+          <td>${result.recipe.totalNutrients.ENERC_KCAL.label}</td>
+          <td>${result.recipe.totalNutrients.ENERC_KCAL.quantity.toFixed()}  ${result.recipe.totalNutrients.ENERC_KCAL.unit}</td>
+        </tr>
+        <tr>
+          <td>${result.recipe.totalNutrients.CA.label}</td>
+          <td>${result.recipe.totalNutrients.CA.quantity.toFixed()} ${result.recipe.totalNutrients.CA.unit}</td>
+        </tr>
+        <tr>
+          <td>${result.recipe.totalNutrients.MG.label}</td>
+          <td>${result.recipe.totalNutrients.MG.quantity.toFixed()} ${result.recipe.totalNutrients.MG.unit}</td>
+        </tr>}
+        <tr>
+          <td>${result.recipe.totalNutrients.FAT.label}</td>
+          <td>${result.recipe.totalNutrients.FAT.quantity.toFixed()}   ${result.recipe.totalNutrients.FAT.unit}</td>
+        </tr>
+        <tr>
+          <td>${result.recipe.totalNutrients.FE.label}</td>
+          <td>${result.recipe.totalNutrients.FE.quantity.toFixed()} ${result.recipe.totalNutrients.FE.unit}</td>
+        </tr>
+       
+      </table>
+    
       <a href="${result.recipe.url}" class="btn btn-primary">Go ${result.recipe.source}</a>
     </div>
     </div>
   `;
 });
 contentEl.innerHTML = cards;
-
-    
-
- 
 }
